587b39c40d6f1541769538147f8f68360598ebae
rascoro1 <andcope1995@gmail.com>
2021-06-15 21:40:14 -0400

    Add result for winners and winning number

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,10 +1,27 @@
 import express from 'express';
+import * as fs from 'fs';
+
+const data_dir = "../data/"
+const winners_file = data_dir + "winners"
+const winning_number_file = data_dir + "winning-number"
+const game_number_file = data_dir + "game-number"
+
 
 const app = express();
 const port = 3000;
 
+function getWinners(): string {
+  return fs.readFileSync(winners_file,'utf8');
+}
+
+function getWinningNumber(): number {
+  return Number(fs.readFileSync(winning_number_file,'utf8'));
+}
+
 app.get('/', (req, res) => {
-  res.send('The Bananumber Game');
+  var winners = getWinners()
+  var winningNumber = getWinningNumber()
+  res.send(`The Bananumber Game\n  Winning Number: ${winningNumber}\n  Winners: ${winners}`);
 });
 
 app.listen(port, () => {
